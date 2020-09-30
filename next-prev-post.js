a851c26f002ca0516d3034093f4838c726a284b4
cena1web <60264674+cena1web@users.noreply.github.com>
2020-09-30 19:21:03 +0300

    Create next-prev-post.js

---
+++ next-prev-post.js
@@ -0,0 +1,6 @@
+var no_image = "http://3.bp.blogspot.com/-Yw8BIuvwoSQ/VsjkCIMoltI/AAAAAAAAC4c/s55PW6xEKn0/s1600-r/nth.png";
+var month_format = [, "كانون2", "شباط", "آذار", "نيسان", "آيار", "حزيران", "تموز", "آب", "أيلول", "تشرين1", "تشرين2", "كانون1"];
+var more_text = "View More";
+var comments_text = "<span>Post </span>Comment";
+var POSTPAGER_OLDER = "السابق <i class='fa fa-hand-o-left'></i>"; // post nav text "previous post"
+var POSTPAGER_NEWER = "<i class='fa fa-hand-o-right'></i> التالي"; // post nav text "next post"
